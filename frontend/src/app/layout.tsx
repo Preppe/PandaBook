b84beef31b8406ce7508b002209775d4528e4572
Giuseppe Fumarola <preppe21@gmail.com>
2025-07-05 19:26:52 +0200

    feat: integrate PWA support and update manifest
    - Added next-pwa package for Progressive Web App support.
    - Created a new manifest.json file with app details and icons.
    - Updated layout.tsx to include manifest link and various meta tags for PWA compatibility.
    - Added new icons for Android and iOS.
    - Removed unused SVG files to clean up the public directory.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="application-name" content="Audiobook App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Audiobook App" />
+        <meta name="description" content="State-of-the-art Next.js starter" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android/android-launchericon-192-192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/android/android-launchericon-512-512.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/ios/180.png" />
       </head>
       <body suppressHydrationWarning={true}>
         <ClientLayout>
